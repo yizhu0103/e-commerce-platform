00bf569027a82edb65253325f473ea39fe8a0051
yizhu0103 <as23063368@gmail.com>
2024-09-19 22:55:33 +0800

    0919

--- js/html.js
+++ js/html.js
@@ -15,37 +15,37 @@
 
 
 
-// $(function () {
+$(function () {
 
-//   // 滑動至指定位置
-//   $('.menulist a').click(function () {/* 物件名稱要抓對不然找不到 */
-//       let menulist = $(this).attr('href');
-//       let loc = $(menulist).offset();/* location */
-//       $('html,body').animate({ scrollTop: loc.top }, 1000);//1000=1秒 幾秒滾過去
-//       /* 任何卷軸的都要去抓html body */
-//   });
+  // 滑動至指定位置
+  $('.menulist a').click(function () {/* 物件名稱要抓對不然找不到 */
+      let menulist = $(this).attr('href');
+      let loc = $(menulist).offset();/* location */
+      $('html,body').animate({ scrollTop: loc.top }, 1000);//1000=1秒 幾秒滾過去
+      /* 任何卷軸的都要去抓html body */
+  });
 
-//   const topbar = document.getElementById('topbar');
-//   const newsection = document.getElementById('new');
-//   const menuOffsetTop = topbar.offsetTop; // 取得清單初始位置
+  const topbar = document.getElementById('topbar');
+  const newsection = document.getElementById('new');
+  const menuOffsetTop = topbar.offsetTop; // 取得清單初始位置
   
-//   window.addEventListener('scroll', () => {
-//       if (window.pageYOffset >= menuOffsetTop) {
-//           topbar.style.position = 'fixed';
-//           topbar.style.top = '0';
-//           topbar.style.zIndex = '999';
-//           topbar.style.width = '100%'; // 保持寬度
-//           newsection.style.marginTop='10vh';
-//       } else {
-//         topbar.style.position = 'static'; // 恢復初始樣式
-//         newsection.style.marginTop='0';
-//       }
-//   });
+  window.addEventListener('scroll', () => {
+      if (window.pageYOffset >= menuOffsetTop) {
+          topbar.style.position = 'fixed';
+          topbar.style.top = '0';
+          topbar.style.zIndex = '999';
+          topbar.style.width = '100%'; // 保持寬度
+          newsection.style.marginTop='10vh';
+      } else {
+        topbar.style.position = 'static'; // 恢復初始樣式
+        newsection.style.marginTop='0';
+      }
+  });
 
   
  
 
-// });
+});
 
 
 
